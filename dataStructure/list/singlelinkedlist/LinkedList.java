0c5cdf526eeb0c17ce34e1244924206fb9dc8fbe
vipulnagar25 <vipulnagar25@gmail.com>
2026-04-23 23:16:30 +0530

    Add files via upload

--- dataStructure/list/singlelinkedlist/LinkedList.java
+++ dataStructure/list/singlelinkedlist/LinkedList.java
@@ -1,119 +1,125 @@
 package dataStructure.list.singlelinkedlist;
 
-import java.util.StringJoiner;
-
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
+import java.util.StringJoiner;
+
 public class LinkedList<E> {
-    Node head;
-    int size;
+  Node head;
+  int size;
 
-    public boolean addAtLast(E e) {
-        Node newNode = new Node(e);
-        if (isNull(head)) {
-            head = newNode;
-        } else {
-            Node currentNode = head;
-            while (nonNull(currentNode.next)) {
-                currentNode = currentNode.next;
-            }
-            currentNode.next = newNode;
-        }
-        size++;
-        return true;
+  public boolean addAtLast(E e) {
+    Node newNode = new Node(e);
+    if (isNull(head)) {
+      head = newNode;
+    } else {
+      Node currentNode = head;
+      while (nonNull(currentNode.next)) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    size++;
+    return true;
+  }
+
+  public boolean addAtFirst(E e) {
+    Node newNode = new Node(e);
+    newNode.next = head;
+    head = newNode;
+    size++;
+    return true;
+  }
+
+  public boolean removeAtFirst() {
+    if (isNull(head)) {
+      return false;
+    }
+    head = head.next;
+    size--;
+    return true;
+  }
+
+  public boolean removeAtLast() {
+    if (isNull(head)) {
+      return false;
+    }
+    if (isNull(head.next)) {
+      head = null;
+      size--;
+      return true;
     }
 
-    public boolean addAtFirst(E e) {
-        Node newNode = new Node(e);
-        newNode.next = head;
-        head = newNode;
-        size++;
-        return true;
+    /* Node currentNode = head;  by two pointer approach
+    Node previous = head;
+    while (nonNull(currentNode.next)) {
+        previous = currentNode;
+        currentNode = currentNode.next;
+    }
+    previous.next = null;
+    size--;
+    return true;*/
+    Node current = head;
+    while (current.next.next != null) {
+      current = current.next;
     }
 
-    public boolean removeAtFirst() {
-        if (isNull(head)) {
-            return false;
-        }
-        head = head.next;
-        size--;
-        return true;
+    current.next = null;
+    size--;
+    return true;
+  }
+
+  public boolean removeAt(int index) {
+    if (size <= index) {
+      return false;
     }
-
-    public boolean removeAtLast() {
-        if (isNull(head)) {
-            return false;
-        }
-        if (isNull(head.next)) {
-            head = null;
-            size--;
-            return true;
-        }
-
-        Node currentNode = head;
-        Node previous = head;
-        while (nonNull(currentNode.next)) {
-            previous = currentNode;
-            currentNode = currentNode.next;
-        }
-        previous.next = null;
-        size--;
-        return true;
+    if (index == 0) {
+      return removeAtFirst();
     }
-
-    public boolean removeAt(int index) {
-        if (size <= index) {
-            return false;
-        }
-        if (index == 0) {
-            return removeAtFirst();
-        }
-        if (index == size - 1) {
-            return removeAtLast();
-        }
-        int count = 0;
-        Node currentNode = head;
-        Node previousNode = head;
-        while (nonNull(currentNode) && (count != index)) {
-            previousNode = currentNode;
-            currentNode = currentNode.next;
-            count++;
-        }
-        previousNode.next = currentNode.next;
-        size--;
-        return true;
+    if (index == size - 1) {
+      return removeAtLast();
     }
-
-    public String toString() {
-        StringJoiner joiner = new StringJoiner(",", "[", "]");
-        Node currentNode = head;
-        while (nonNull(currentNode)) {
-            joiner.add(String.valueOf(currentNode.data));
-            currentNode = currentNode.next;
-        }
-        return joiner.toString();
+    int count = 0;
+    Node currentNode = head;
+    Node previousNode = head;
+    while (nonNull(currentNode) && (count != index)) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+      count++;
     }
+    previousNode.next = currentNode.next;
+    size--;
+    return true;
+  }
 
-    public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.addAtLast(1);
-        linkedList.addAtLast(2);
-        linkedList.addAtFirst(0);
-        linkedList.addAtLast(3);
-        linkedList.addAtFirst(-1);
-        System.out.println(linkedList.size);
-        System.out.println(linkedList);
-//        linkedList.removeAtFirst();
-//        System.out.println(linkedList.size);
-//        System.out.println(linkedList);
-//        linkedList.removeAtLast();
-//        System.out.println(linkedList.size);
-//        System.out.println(linkedList);
-        linkedList.removeAt(4);
-        System.out.println(linkedList.size);
-        System.out.println(linkedList);
-
+  public String toString() {
+    StringJoiner joiner = new StringJoiner(",", "[", "]");
+    Node currentNode = head;
+    while (nonNull(currentNode)) {
+      joiner.add(String.valueOf(currentNode.data));
+      currentNode = currentNode.next;
     }
+    return joiner.toString();
+  }
 
+  public static void main(String[] args) {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.addAtLast(1);
+    linkedList.addAtLast(2);
+    linkedList.addAtFirst(0);
+    linkedList.addAtLast(3);
+    linkedList.addAtFirst(-1);
+    System.out.println(linkedList.size);
+    System.out.println(linkedList);
+    //        linkedList.removeAtFirst();
+    //        System.out.println(linkedList.size);
+    //        System.out.println(linkedList);
+    linkedList.removeAtLast();
+    //        System.out.println(linkedList.size);
+    //        System.out.println(linkedList);
+    // linkedList.removeAt(4);
+    System.out.println(linkedList.size);
+    System.out.println(linkedList);
+  }
 }
